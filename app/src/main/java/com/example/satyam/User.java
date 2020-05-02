136675e1e002e124d246ee95b74b0d665fd7e18f
Satyam Atasnia <satyamatasnia@gmail.com>
2020-05-02 14:32:24 +0530

    f

---
+++ app/src/main/java/com/example/satyam/User.java
@@ -0,0 +1,31 @@
+package com.example.satyam;
+
+
+class User {
+    String Displayname;
+
+
+    String Email;
+    long createdAt;
+
+    public User (){};
+    public User(String displayname,String email,long createdAt){
+        this.Displayname=displayname;
+       this.Email=email;
+        this.createdAt=createdAt;
+    }
+
+
+    public String getDisplayname() {
+        return Displayname;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+}
